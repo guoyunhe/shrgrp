776ae1aeaf1e70c1751bbeb6d544da412a334096
Guo Yunhe <guoyunhebrave@gmail.com>
2017-04-03 14:16:07 +0300

    indent size to 4

--- public/javascripts/app.ts
+++ public/javascripts/app.ts
@@ -7,7 +7,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 $(function () {
-  platformBrowserDynamic().bootstrapModule(AppModule);
+    platformBrowserDynamic().bootstrapModule(AppModule);
 });
 
 console.log('hello'); // hello
